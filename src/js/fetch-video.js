b63baa8d35b23891c374dc352fee3630b1d78c6e
Emil Szymczyk <106548124+SzEmil@users.noreply.github.com>
2022-12-28 14:24:15 +0100

    Api search video bar

--- src/js/fetch-video.js
+++ src/js/fetch-video.js
@@ -1,5 +1,34 @@
-"use strict"
+'use strict';
 
-function fetchVideo() {
-    
-}
+// export const fetchVideo = async (name, pageNumber) => {
+//   const response = await fetch();
+//   const videos = await response.json(
+//     `https://api.themoviedb.org/3/search/movie?api_key=5349b69c770fce41df09c49c43dbcd6b&language=en-US&query=${name}&page=${pageNumber}&include_adult=true`
+//   );
+
+//   return videos;
+// };
+
+// export const fetchVideoPopular = async () => {
+//   const response = await fetch();
+//   const videos = await response.json(
+//     `https://api.themoviedb.org/3/trending/movie/week?api_key=5349b69c770fce41df09c49c43dbcd6b`
+//   );
+
+//   return videos;
+// };
+
+export const fetchVideo = (name, pageNumber) => {
+  return fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=5349b69c770fce41df09c49c43dbcd6b&language=en-US&query=${name}&page=${pageNumber}&include_adult=true`
+  )
+    .then(response => {
+      if (!response) {
+        throw new Error('Failed to fetch video API');
+      }
+      return response.json();
+    })
+    .catch(error => {
+      console.error(error);
+    });
+};
--- src/js/home-library.js
+++ src/js/home-library.js
@@ -1,3 +1,54 @@
-'use strict';
+const sacas = 'ass';
+('use strict');
+import { fetchVideo } from './fetch-video';
+import { fetchVideoPopular } from './fetch-video';
 
-console.log('hejo');
+console.log('hejo bla bla');
+const form = document.querySelector('#search-form');
+const gallery = document.querySelector('.home-gallery');
+let pageNumber = 1;
+
+const searchVideo = event => {
+  event.preventDefault();
+
+  const {
+    elements: { searchQuery },
+  } = form;
+
+  const formSearch = searchQuery.value;
+  console.log(formSearch);
+  fetchVideo(formSearch, pageNumber)
+    .then(data => {
+      const dataArray = data.results;
+      console.log(dataArray);
+      gallery.innerHTML = ``;
+      renderVideoCard(dataArray);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+};
+
+const renderVideoCard = videoArray => {
+  videoArray.map(video => {
+    const card = document.createElement('div');
+    card.innerHTML = `<div class = "home-gallery__card"><img class="home-gallery__img" src="https://image.tmdb.org/t/p/w300${video.poster_path}" alt ="video poster"><h3 class= "home-gallery-title">${video.title}</h3><a class = "home-gallery-link">kategoria|data</a></div>`;
+
+    gallery.appendChild(card);
+  });
+};
+
+// const generatePopularVideos = async event => {
+//   try {
+//     const name = 'spiderman';
+//     const pageNumber = 1;
+//     // event.preventDefault();
+//     console.log('fetchujemy filmy');
+//     const videos = await fetchVideoPopular();
+//     console.log(videos);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
+
+form.addEventListener('submit', searchVideo);
