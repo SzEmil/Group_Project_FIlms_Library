327d74f68ddd6ac3c91b03242d1f157ea53ce6af
taaleksandra <olatarnas@gmail.com>
2022-12-30 17:46:21 +0100

    popular movies

--- src/js/fetch-video.js
+++ src/js/fetch-video.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// zapytanie o film zgodny z wartością przekazaną w input
 export const fetchVideo = async (name, pageNumber) => {
   const response = await fetch(
     `https://api.themoviedb.org/3/search/movie?api_key=5349b69c770fce41df09c49c43dbcd6b&language=en-US&query=${name}&page=${pageNumber}&include_adult=true`
@@ -9,6 +10,7 @@ export const fetchVideo = async (name, pageNumber) => {
   return videos;
 };
 
+// zapytanie o najpopularniejsze filmy w tym tygodniu
 export const fetchVideoPopular = async () => {
   const response = await fetch(
     `https://api.themoviedb.org/3/trending/movie/week?api_key=5349b69c770fce41df09c49c43dbcd6b`
--- src/js/home-library.js
+++ src/js/home-library.js
@@ -6,7 +6,41 @@ import { fetchVideoPopular } from './fetch-video';
 const form = document.querySelector('#search-form');
 const gallery = document.querySelector('.home-gallery');
 let pageNumber = 1;
-let boject = {};
+// let boject = {};
+
+// funkcja do wyświetlania wyszukanych filmów
+const renderVideoCard = videoArray => {
+  videoArray.map(video => {
+    const releaseDate = video.release_date;
+    const movieYear = releaseDate.split('-');
+
+    const arrGenreId = video.genre_ids;
+    const genreId = arrGenreId.join(', ');
+
+    const card = document.createElement('div');
+    card.innerHTML = `<div class = "home-gallery__card"><img class="home-gallery__img" src="https://image.tmdb.org/t/p/w300${video.poster_path}" alt ="video poster"><h3 class= "home-gallery-title">${video.title}</h3><p class = "home-gallery-link">${genreId} | ${movieYear[0]}</p></div>`;
+
+    gallery.appendChild(card);
+  });
+};
+
+// obsługa zapytania o najpopularniejsze filmy
+const loadPopularMovies = event => {
+  gallery.innerHTML = ``;
+
+  fetchVideoPopular()
+    .then(data => {
+      console.log(data);
+      const dataArray = data.results;
+      console.log(dataArray);
+      renderVideoCard(dataArray);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+};
+
+// obsługa zapytania o film dopasowany do wartości wpisanej do input
 const searchVideo = async event => {
   event.preventDefault();
 
@@ -16,37 +50,22 @@ const searchVideo = async event => {
 
   const formSearch = searchQuery.value;
   console.log(formSearch);
+
   await fetchVideo(formSearch, pageNumber)
     .then(data => {
+      gallery.innerHTML = ``;
       const dataArray = data.results;
       console.log(dataArray);
-      gallery.innerHTML = ``;
       renderVideoCard(dataArray);
-      boject = data;
+      // boject = data;
     })
     .catch(error => {
       console.error(error);
     });
 };
 
-const renderVideoCard = videoArray => {
-  videoArray.map(video => {
-    const card = document.createElement('div');
-    card.innerHTML = `<div class = "home-gallery__card"><img class="home-gallery__img" src="https://image.tmdb.org/t/p/w300${video.poster_path}" alt ="video poster"><h3 class= "home-gallery-title">${video.title}</h3><a class = "home-gallery-link">kategoria|data</a></div>`;
+// tworzenie siatki popularnych filmów przy załadowaniu strony
+window.onload = loadPopularMovies;
 
-    gallery.appendChild(card);
-  });
-};
-
-// const generatePopularVideos = async event => {
-//   try {
-//     // event.preventDefault();
-//     console.log('fetchujemy filmy');
-//     const videos = await fetchVideoPopular();
-//     console.log(videos);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
-
+// tworzenie siatki filmów odpowiadających wartości input
 form.addEventListener('submit', searchVideo);
