4cf3f83c6a2b7012c863f71f3c1e7ff2aab2f541
Gosia Antoniak/gosia-antoniak_89@o2.pl <gosia-antoniak_89@o2.pl>
2023-01-03 15:03:50 +0100

    feat:  my_library button 'watched' and 'queue' in JS

--- src/js/my-library.js
+++ src/js/my-library.js
@@ -2,8 +2,9 @@ import { renderModal } from './more-info-modal';
 import { fetchDetails } from './fetch-video';
 
 const ACTIVE_SECTION_CLASS = 'active';
-const WATCHED_SECTION_ID = 'my-library-watched';
-const QUEUE_SECTION_ID = 'my-library-queue';
+const HIDDEN_SECTION_CLASS = 'hidden';
+const WATCHED_SECTION_ID = 'filmoteka-watched';
+const QUEUE_SECTION_ID = 'filmoteka-queue';
 const LOCAL_STORAGE_WATCHED_KEY = 'filmoteka-watched';
 const LOCAL_STORAGE_QUEUE_KEY = 'filmoteka-queue';
 const sectionButtons = document.querySelectorAll('.my-library-space__btn');
@@ -20,20 +21,40 @@ function initListeners() {
     const isActiveButton = e.target.classList.contains('active');
     if (!isActiveButton) {
       markActiveButton(e.target);
-      populateSection(e);
+      populateSection(e.target);
     }
   }));
 }
 
-function populateSection(event) {
-  sectionContainer.innerHTML = null;
-  const selectedSectionId = event?.target.getAttribute('id') ?? WATCHED_SECTION_ID;
-  const elements = JSON.parse(readSectionFromLocalStorage(selectedSectionId));
-  if (elements?.length > 0) {
-    elements.forEach((movieData) => renderMovieElement(movieData));
-  } else {
-    // tutaj renderujemy page z brakiem filmów
-  }
+export function populateSection(target) {
+  sectionContainer.classList.add(HIDDEN_SECTION_CLASS);
+  const timeout = setTimeout(() => {
+    sectionContainer.innerHTML = null;
+    const selectedSectionId = target?.getAttribute('id') ?? WATCHED_SECTION_ID;
+    const elements = JSON.parse(readSectionFromLocalStorage(selectedSectionId));
+    if (elements?.length > 0) {
+      elements.forEach((movieData) => renderMovieElement(movieData));
+    } else {
+      renderEmptyState();
+    }
+    sectionContainer.classList.remove(HIDDEN_SECTION_CLASS);
+    clearTimeout(timeout);
+  }, 300);
+}
+
+function renderEmptyState() {
+  const markup = `
+  <li class='grid_my_library__element-empty'>
+      <p class='grid_my_library__element-empty-txt'>
+        The Movie Dog<br/>
+        <strong>is waiting for you</strong><br/>
+        to add some movies<br/>
+        to the list
+      </p>
+        <div class='grid_my_library__element-empty-img'></div>
+    </li>
+  `;
+  sectionContainer.innerHTML = markup;
 }
 
 function readSectionFromLocalStorage(sectionName) {
@@ -56,11 +77,10 @@ function readElementsFromLocalStorage(key) {
 
 }
 
-function getDetails (id) {
+function getDetails(id) {
 
   fetchDetails(id)
     .then(data => {
-      console.log(data);
       renderModal(data);
     })
     .catch(error => {
@@ -78,17 +98,19 @@ function renderMovieElement(movieData) {
     imageSrc,
     id,
   } = movieData;
-  const categoriesText = categories.join(', ');
   const listElement = document.createElement('li');
   const buttonElement = document.createElement('button');
+  const categoriesMarkup = categories ? `<span class='grid_my_library__element-category'>${categories}</span>` : '';
+  const yearMarkup = categories ? `<span class='grid_my_library__element-year'>${year}</span>` : '';
+  const scoreMarkup = categories ? `<span class='grid_my_library__element-score'>${score}</span>` : '';
   const buttonMarkup = `
         <img src='${imageSrc}'
              class='grid_my_library__element-img'
              alt='${title}' />
         <span class='grid_my_library__element-title'>${title}</span><br/>
-        <span class='grid_my_library__element-category'>${categoriesText}</span>
-        <span class='grid_my_library__element-year'>${year}</span>
-        <span class='grid_my_library__element-score'>${score}</span>
+        ${categoriesMarkup}
+        ${yearMarkup}
+        ${scoreMarkup}
   `;
   buttonElement.setAttribute('role', 'button');
   buttonElement.setAttribute('title', title);
@@ -105,4 +127,6 @@ function initScripts() {
   populateSection();
 }
 
-initScripts();
+if (sectionButtons.length > 0) {
+  initScripts();
+}
