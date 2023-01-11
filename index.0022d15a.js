41bcb96a237c7dbc3ddde814e0132169e9f32b3f
SzEmil <106548124+SzEmil@users.noreply.github.com>
2023-01-11 16:57:35 +0000

    Deploying to gh-pages from @ SzEmil/Group_Project_FIlms_Library@dddd763f1db4f306e4fec7ea0652667045dfdd62 🚀

---
+++ index.0022d15a.js
@@ -0,0 +1,2 @@
+const o=document.getElementById("scroll-to-top-button");window.onscroll=function(){document.body.scrollTop>20||document.documentElement.scrollTop>20?o.classList.add("show"):o.classList.remove("show")},o.onclick=function(){window.scroll({top:0,left:0,behavior:"smooth"})};
+//# sourceMappingURL=index.0022d15a.js.map
