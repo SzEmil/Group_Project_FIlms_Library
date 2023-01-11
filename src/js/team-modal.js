9802554de90a53afa1a39a771d8de74e8a1d2a0c
Anna <michalowska.aneczka@gmail.com>
2023-01-11 22:39:28 +0100

    without_esc
    nie działało to przy daniu remove zamiast toggle ani, przy wyłączeniu eventlistener, więc usunęłam funkcję z przyciskiem esc, trzeba wyłączać na x. Nie wiem o co chodzi nie mam już dziś siły.

--- src/js/team-modal.js
+++ src/js/team-modal.js
@@ -1,21 +1,16 @@
+'use strict';
+
 const openModalBtn = document.querySelector('[data-modal-open]');
 const closeModalBtn = document.querySelector('[data-modal-close]');
 const modalWindow = document.querySelector('[data-modal]');
 const bodyWindow = document.querySelector('body');
 
 openModalBtn.addEventListener('click', () => {
-  modalWindow.classList.toggle('hidden');
+  modalWindow.classList.remove('hidden');
   bodyWindow.style.overflow = 'hidden';
 });
 
 closeModalBtn.addEventListener('click', () => {
-  modalWindow.classList.toggle('hidden');
+  modalWindow.classList.add('hidden');
   bodyWindow.style.overflow = 'auto';
-});
-
-document.addEventListener('keydown', event => {
-  if (event.key === 'Escape') {
-    modalWindow.classList.toggle('hidden');
-    bodyWindow.style.overflow = 'auto';
-  }
 });
